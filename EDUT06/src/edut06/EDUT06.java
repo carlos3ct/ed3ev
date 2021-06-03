451c74d54e540b16fa2604fb88167bdec6204a3d
mcruz <mcruz@MCACER>
2021-06-04 01:33:57 +0200

    Versión 1.2. Mostrar mensajes

--- EDUT06/src/edut06/EDUT06.java
+++ EDUT06/src/edut06/EDUT06.java
@@ -15,7 +15,8 @@ public class EDUT06 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola, mundo");
+        System.out.println("Ya estamos muy cansados");
     }
     
 }
